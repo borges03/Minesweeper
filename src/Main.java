b712634bad8c854362c0f80d01527d7a9950afb7
Genesis Borges <genesis.borges@upr.edu>
2018-03-22 00:37:05 -0400

    Update Main.java
    Mines placed randomly

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.awt.Color;
+import java.util.Random;
 import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Bienve's Grid");
+		JFrame myFrame = new JFrame("Minesweeper Game");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
 		myFrame.setSize(900, 900);
@@ -14,5 +16,14 @@ public class Main {
 		myFrame.addMouseListener(myMouseAdapter);
 
 		myFrame.setVisible(true);
+		Random a = new Random();
+		Random b = new Random();
+		
+	for(int i = 0;i<=10;i++){
+			int rx = a.nextInt(9);
+			int ry = b.nextInt(9);
+			myPanel.colorArray[rx][ry] = Color.BLACK;
+			myPanel.isMine[rx][ry] = true; 
+			}
 	}
 }
